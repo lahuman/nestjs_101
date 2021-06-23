5a3625506732ef5145a704d3af30460909d1a5ab
Daniel Lim <lahuman@daum.net>
2021-06-23 14:25:53 +0900

    feat : testcase

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,86 @@
+import { CanActivate, ExecutionContext, INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import * as request from 'supertest';
+import { AuthenticatedGuard } from '../common/guards/authenticated.guard';
+import { UserEntity } from '../entity/user.entity';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+
+const mockUser = (param) => new UserEntity({ seq: 1, id: 'lahuman', name: '임광규', email: 'lahuman@daum.net', passowrd: '1234', ...param });
+
+class MockRepository {
+  async findOne(param) {
+    return mockUser(param);
+  }
+  async find(condition) {
+    return [mockUser(condition)];
+  }
+  async softDelete(id) {
+
+  }
+  async save(userInfo: UserEntity) {
+    return mockUser(userInfo);
+  }
+}
+
+const mockAuthGuard: CanActivate = {
+  canActivate: (context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    request.user = {
+      id: 'lahuman',
+      name: "임광규",
+      email: 'lahuman@daum.net'
+    };
+    return request.user;
+  }
+}
 
 describe('UserController', () => {
+  let app: INestApplication;
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-    }).compile();
+      providers: [UserService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useClass: MockRepository,
+        },
+      ],
+    })
+      .overrideGuard(AuthenticatedGuard).useValue(mockAuthGuard)
+      .compile();
 
+    app = module.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
     controller = module.get<UserController>(UserController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  
+  it('사용자 조회', async () => {
+    const response = await request(app.getHttpServer())
+      .get("/user/1")
+      .expect(200);
+    expect(JSON.parse(response.text).user.id).toEqual('lahuman');
+  });
+
+  it('사용자 추가', async () => {
+    const response = await request(app.getHttpServer())
+      .post("/user")
+      .send({
+        id: 'lahuman',
+        name: '임광규',
+        email: 'lahuman@daum.net',
+        password: '1234'
+      })
+      .expect(201);
+    expect(JSON.parse(response.text).user.id).toEqual('lahuman');
   });
 });
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,14 +8,19 @@ import {
   Post,
   Put,
   Query,
+  UseFilters,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthExceptionFilter } from '../common/filters/auth-exceptions.filter';
+import { AuthenticatedGuard } from '../common/guards/authenticated.guard';
 import { CreateUserDto, ModifyUserDto, SearchUserDto, UserRO } from './dto/user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 @ApiTags('사용자')
+@UseFilters(AuthExceptionFilter)
 export class UserController {
   constructor(private readonly service: UserService) { }
 
@@ -30,6 +35,7 @@ export class UserController {
   @ApiOperation({ summary: '사용자 목록 조회' })
   @ApiResponse({ status: 200, type: UserRO })
   @UseInterceptors(ClassSerializerInterceptor)
+  @UseGuards(AuthenticatedGuard)
   @Get(':id')
   async findUser(@Param("id") id: string): Promise<UserRO> {
     return this.service.findUser(id);
