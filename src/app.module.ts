93f1507b9874258fedb0ebbd976c49aa017def57
Daniel Lim <k.lim@samsung.com>
2022-04-15 20:51:51 +0900

    feat : @nestjs/config 를 활용한 상수 처리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,92 @@
-import { HttpModule, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import {
+  HttpModule,
+  Logger,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+} from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AppLoggerMiddleware } from './common/middleware/AppLoggerMiddleware';
-
+import { WinstonModule } from 'nest-winston';
+import * as Transport from 'winston-transport';
+import * as winston from 'winston';
+import * as path from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot(), HttpModule,
-  TypeOrmModule.forRoot({
-    type: "sqlite",
-    database: ":memory:",
-    dropSchema: true,
-    entities: ["dist/**/*.entity{.ts,.js}"],
-    synchronize: true,
-    logging: true
-  }),
+  imports: [
+    ConfigModule.forRoot(),
+    HttpModule,
+    WinstonModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const transports: [Transport] = [
+          new (require('winston-daily-rotate-file'))({
+            dirname: path.join(
+              __dirname,
+              configService.get('LOGGING_PATH'),
+              '/info/',
+            ),
+            filename: 'info-%DATE%.log',
+            datePattern: 'YYYY-MM-DD-HH',
+            level: 'info',
+            zippedArchive: configService.get('LOGGING_ZIP') === 'true',
+            maxSize: configService.get('LOGGING_MAXSIZE'),
+            maxFiles: configService.get('LOGGING_MAXFILES'),
+          }),
+        ];
+
+        // LOGGING_DEBUG 가 true 일 경우만 추가
+        if (configService.get('LOGGING_DEBUG') === 'true') {
+          transports.push(
+            new (require('winston-daily-rotate-file'))({
+              dirname: path.join(
+                __dirname,
+                configService.get('LOGGING_PATH'),
+                '/debug/',
+              ), // 파일 저장 위치
+              filename: 'debug-%DATE%.log', // 파일 명
+              datePattern: 'YYYY-MM-DD-HH', // 파일명의 날짜(DATE) 패턴
+              level: 'debug', // 로그 레벨
+              zippedArchive: configService.get('LOGGING_ZIP') === 'true', //압축 여부
+              maxSize: configService.get('LOGGING_MAXSIZE'), // 한개의 파일 최대 크기
+              maxFiles: configService.get('LOGGING_MAXFILES'), // 파일의 최대 유지 날짜
+            }),
+          );
+        }
+
+        return {
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.json(),
+          ),
+          transports: [
+            new winston.transports.Console({
+              level: configService.get('LOGGING_CONSOLE_LEVEL'),
+            }),
+            ...transports,
+          ],
+        };
+      },
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'sqlite',
+        database: configService.get('DB_HOST'),
+        dropSchema: configService.get('DB_DROP') === 'true',
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: configService.get('DB_SYNC') === 'true',
+        logging: configService.get('DB_LOGGING') === 'true',
+        logger: 'file',
+      }),
+    }),
     UserModule,
     AuthModule,
   ],
